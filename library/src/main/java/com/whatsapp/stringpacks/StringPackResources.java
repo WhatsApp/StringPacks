093a99aa095c1db066d9bb80eb3b7b4403647cdc
Ashish Gupta <agupta82@fb.com>
2021-06-01 12:53:32 -0700

    Upgrading the library's compileSdkVersion to 30
    Summary: Updating the library to API 30 and also added support for methods added in API 30
    
    Differential Revision: D28661535
    
    fbshipit-source-id: f6e0e82

--- library/src/main/java/com/whatsapp/stringpacks/StringPackResources.java
+++ library/src/main/java/com/whatsapp/stringpacks/StringPackResources.java
@@ -12,12 +12,14 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.content.res.XmlResourceParser;
+import android.content.res.loader.ResourcesLoader;
 import android.graphics.Movie;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import java.io.IOException;
 import java.io.InputStream;
@@ -291,6 +293,18 @@ public class StringPackResources extends Resources {
   public void parseBundleExtra(String tagName, AttributeSet attrs, Bundle outBundle)
       throws XmlPullParserException {
     baseResources.parseBundleExtra(tagName, attrs, outBundle);
+  }
+
+  @RequiresApi(30)
+  @Override
+  public void addLoaders(@NonNull final ResourcesLoader... loaders) {
+    baseResources.addLoaders(loaders);
+  }
+
+  @RequiresApi(30)
+  @Override
+  public void removeLoaders(@NonNull final ResourcesLoader... loaders) {
+    baseResources.removeLoaders(loaders);
   }
 
   // endregion
