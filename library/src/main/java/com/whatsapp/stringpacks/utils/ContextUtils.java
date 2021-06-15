cc4b13a41789ff770422ea0b2d577cae9daf4623
Ashish Gupta <agupta82@fb.com>
2021-06-15 10:56:16 -0700

    Adding Copyright to missing file(s)
    Summary: ^
    
    Differential Revision: D29072919
    
    fbshipit-source-id: a724d7a

--- library/src/main/java/com/whatsapp/stringpacks/utils/ContextUtils.java
+++ library/src/main/java/com/whatsapp/stringpacks/utils/ContextUtils.java
@@ -1,3 +1,9 @@
+/* Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
+
 package com.whatsapp.stringpacks.utils;
 
 import android.content.Context;
